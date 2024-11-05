2260f969320268cf79ca65924470b5565da51ce7
박헌 <ph031@naver.com>
2024-11-05 17:02:32 +0900

    feat: Color 관리 생성 및 로그인 화면 기틀

---
+++ front/lib/theme/color.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+const White = Color(0xffFFFFFF);
+const Gray  = Color(0xff989898);
+const VeryDarkGray = Color(0xff3A3A3A);
+const Black = Color(0xff000000);
+const StrokeBlack = Color(0x66000000);
+const PureRed = Color(0xffEA0029);
+const AsteriskPureRed = Color(0xb3EA0029);
+const Beige = Color(0xffF8F5F0);
+const Latte = Color(0xffD7C3A8);
+const Green = Color(0xffACC270);
+const SeaGreen = Color(0xff669358);
+const Olive = Color(0xff7C812F);
+const YellowOlive = Color(0xffCBC273);
+const Yellow = Color(0xffF8EAAB);
+const Orange = Color(0xffFEC868);
+const Merigold = Color(0xffFFC65D);
+const tangereine = Color(0xffFDA868);
+const Brown = Color(0xff463C33);
+const PureOrange = Color(0xfff4a700);
+const DarkGrayishOrange = Color(0xff75695f);
+const MostlyDesaturatedDarkOrange = Color(0xffa48365);
