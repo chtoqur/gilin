34f8b9d62ed14496176c8c5a63b6cf30698ace73
seungki-cho <sndkqkfk7@gmail.com>
2024-11-08 20:45:22 +0900

    feat: Coordinate 좌표 차이 method

--- backend/route/src/main/java/com/gilin/route/domain/bus/dto/Coordinate.java
+++ backend/route/src/main/java/com/gilin/route/domain/bus/dto/Coordinate.java
@@ -4,4 +4,9 @@ public record Coordinate(
         Double latitude,
         Double longitude
 ) {
+    public double distanceFrom(Coordinate other) {
+        double latDiff = other.latitude - latitude;
+        double lngDiff = other.longitude - longitude;
+        return Math.sqrt(Math.pow(latDiff, 2) + Math.pow(lngDiff, 2));
+    }
 }
