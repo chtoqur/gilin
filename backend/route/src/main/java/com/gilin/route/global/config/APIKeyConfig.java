31aeef114c681fb6dc8898083bf644a1bfc9cf11
jongho <whdghthdms44@gmail.com>
2024-11-12 17:15:37 +0900

    chore: API KEY tmap 코드 중복 제거

--- backend/route/src/main/java/com/gilin/route/global/config/APIKeyConfig.java
+++ backend/route/src/main/java/com/gilin/route/global/config/APIKeyConfig.java
@@ -7,20 +7,15 @@ import org.springframework.stereotype.Component;
 public class APIKeyConfig {
 
     private final String[] ODSayKeys;
-    private final String[] TMapKeys;
 
-    public APIKeyConfig(@Value("${apikey.odsays}") String[] ODSayKeys,
-        @Value("${apikey.tmap}") String[] TMapKeys) {
+    public APIKeyConfig(@Value("${apikey.odsays}") String[] ODSayKeys
+    ) {
         this.ODSayKeys = ODSayKeys;
-        this.TMapKeys = TMapKeys;
     }
 
     public String getODSayKey() {
         return ODSayKeys[0];
-        
+
     }
 
-    public String getTMapKey() {
-        return TMapKeys[0];
-    }
 }
