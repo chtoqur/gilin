6eed39eb46a9c6d86be77217ad195895945da6e4
박헌 <ph031@naver.com>
2024-11-12 14:16:27 +0900

    feat:스플래시 스크린, 로그인 페이지 생성

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'dart:developer';
 import 'package:gilin/state/router/router_provider.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
+
 
 void main() async {
   await _initialize();
@@ -16,9 +18,16 @@ void main() async {
 
 Future<void> _initialize() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 네이버맵 초기화
   await NaverMapSdk.instance.initialize(
       clientId: 'w5yq6v1x69',
       onAuthFailed: (e) => log("네이버맵 인증오류: $e", name: "onAuthFailed"));
+
+  // 카카오 SDK 초기화
+  KakaoSdk.init(
+    nativeAppKey: '7e758a049b329a8bab71071c8ab8cb02',
+  );
 }
 
 class MyApp extends ConsumerWidget {
--- front/lib/screens/mypage/mypage_screen.dart
+++ front/lib/screens/mypage/mypage_screen.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gilin/state/navigation/navigation_provider.dart';
 
-class MypageScreen extends StatelessWidget {
+class MypageScreen extends ConsumerWidget {
   const MypageScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Mypage Home Screen'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Schedule my page Screen'),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () => ref
+                .read(bottomNavProvider.notifier)
+                .navigateToPage(context, 0, '/login'),
+            child: const Text('Go to Login'),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () => ref
+                .read(bottomNavProvider.notifier)
+                .navigateToPage(context, 3, '/signup'),
+            child: const Text('Go to Signup'),
+          ),
+        ],
+      ),
     );
   }
 }
