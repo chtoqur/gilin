054b386d34fd06d0c9c8dee5ea3d2fbf86aa228c
seungki-cho <sndkqkfk7@gmail.com>
2024-11-12 15:43:14 +0900

    chore: 오타 수정

---
+++ backend/route/src/main/java/com/gilin/route/domain/member/controller/MemberController.java
@@ -0,0 +1,32 @@
+package com.gilin.route.domain.member.controller;
+
+import com.gilin.route.domain.member.dto.OAuthTokenRequest;
+import com.gilin.route.domain.member.dto.TokenResponse;
+import com.gilin.route.domain.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/login")
+    @Operation(description = "OAuth 에서 받은 Token을 사용해 로그인/회원가입 합니다.", summary = "OAuth 에서 받은 Token을 사용해 로그인/회원가입 합니다.")
+    public ResponseEntity<TokenResponse> oauthLogin(@RequestBody OAuthTokenRequest oAuthTokenRequest) {
+        return ResponseEntity.ok(memberService.login(oAuthTokenRequest));
+    }
+
+}
