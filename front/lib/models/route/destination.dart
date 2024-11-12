471da5e4edcc38eb3d9ed9f2f6d54baa110b31f5
최이서 <chxiseo@naver.com>
2024-11-12 11:01:02 +0900

    feat: 메인 페이지 ui 수정 완료

--- front/lib/models/route/destination.dart
+++ front/lib/models/route/destination.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Destination {
-  final IconData icon;
+  final String iconPath;
   final String name;
   final String address;
   final String arrivalTime;
 
   const Destination({
-    required this.icon,
+    required this.iconPath,
     required this.name,
     required this.address,
     required this.arrivalTime,
--- front/lib/state/route/route_state.dart
+++ front/lib/state/route/route_state.dart
@@ -1,4 +1,3 @@
-// route_state.dart
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class RouteLocation {
