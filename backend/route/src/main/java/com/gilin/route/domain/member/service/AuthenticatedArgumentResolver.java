e88112a3571ca05632aabb0175ece25e82f0c700
seungki-cho <sndkqkfk7@gmail.com>
2024-11-13 11:20:15 +0900

    feat: 멤버 Argument Resolver

---
+++ backend/route/src/main/java/com/gilin/route/domain/member/service/AuthenticatedArgumentResolver.java
@@ -0,0 +1,45 @@
+package com.gilin.route.domain.member.service;
+
+import com.gilin.route.domain.member.dto.Authenticated;
+import com.gilin.route.domain.member.repository.MemberRepository;
+import com.gilin.route.global.error.GilinException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.Objects;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticatedArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(Authenticated.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+        Long memberId = parseMemberId(webRequest.getHeader("AUTHORIZATION"));
+        return Objects.isNull(memberId) ? null : memberRepository.findById(memberId).orElse(null);
+    }
+
+    private Long parseMemberId(String token) {
+        if (Objects.isNull(token) || token.startsWith("Bearer ")) {
+            return null;
+        }
+        return Long.parseLong(jwtTokenProvider.extractAccessToken(token.substring(7)));
+    }
+}
---
+++ backend/route/src/main/java/com/gilin/route/global/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.gilin.route.global.config;
+
+import com.gilin.route.domain.member.service.AuthenticatedArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@EnableWebMvc
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final AuthenticatedArgumentResolver authenticatedArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        WebMvcConfigurer.super.addArgumentResolvers(resolvers);
+        resolvers.add(authenticatedArgumentResolver);
+    }
+}
