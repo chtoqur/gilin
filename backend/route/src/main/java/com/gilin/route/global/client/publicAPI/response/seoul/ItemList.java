b1362892bc1533e4844dd9f95bd6025fd6e4599e
조승기 <beenzino@kookmin.ac.kr>
2024-11-15 11:09:43 +0900

    feat: 서울 버스 feign client

---
+++ backend/route/src/main/java/com/gilin/route/global/client/publicAPI/response/seoul/ItemList.java
@@ -0,0 +1,26 @@
+package com.gilin.route.global.client.publicAPI.response.seoul;
+
+public record ItemList(
+    String arrmsg1,
+    String arrmsg2,
+    String arsId,
+    String busRouteAbrv,
+    String busRouteId,
+    String busType1,
+    String busType2,
+    String deTourAt,
+    String firstTm,
+    String lastTm,
+    String mkTm,
+    String nextBus,
+    String routeType,
+    String rtNm,
+    String stId,
+    String stNm,
+    String staOrd,
+    String stationNm1,
+    String stationNm2,
+    String term,
+    String vehId1,
+    String vehId2
+) {}
