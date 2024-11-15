9c7e1fad2b9e5419ee0f6df7688b3f80d68a3b4e
조승기 <beenzino@kookmin.ac.kr>
2024-11-15 22:51:48 +0900

    feat: 서울 버스 API 추가

--- backend/route/src/main/java/com/gilin/route/global/client/publicAPI/SeoulBusArrivalClient.java
+++ backend/route/src/main/java/com/gilin/route/global/client/publicAPI/SeoulBusArrivalClient.java
@@ -1,15 +1,21 @@
 package com.gilin.route.global.client.publicAPI;
 
 import com.gilin.route.global.client.publicAPI.request.SeoulBusArrivalRequest;
+import com.gilin.route.global.client.publicAPI.request.SeoulBusStationArrivalRequest;
+import com.gilin.route.global.client.publicAPI.response.seoul.LowArrInfoItemList;
 import com.gilin.route.global.client.publicAPI.response.seoul.ServiceResult;
+import com.gilin.route.global.client.publicAPI.response.seoul.StationItemList;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(url = "http://ws.bus.go.kr/api/rest/arrive" , name = "BusClient1")
+@FeignClient(url = "http://ws.bus.go.kr/api/rest" , name = "SeoulBusArrivalClient")
 public interface SeoulBusArrivalClient {
-    @GetMapping("/getLowArrInfoByStId")
-    ServiceResult getArrInfoByRouteAll(@SpringQueryMap SeoulBusArrivalRequest seoulBusArrivalRequest);
+    @GetMapping("/arrive/getLowArrInfoByStId")
+    ServiceResult<LowArrInfoItemList> getArrInfoByRouteAll(@SpringQueryMap SeoulBusArrivalRequest seoulBusArrivalRequest);
+
+    @GetMapping("/stationinfo/getStationByUid")
+    ServiceResult<StationItemList> getStationByUid(@SpringQueryMap SeoulBusStationArrivalRequest seoulBusStationArrivalRequest);
 }
