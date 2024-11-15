1c0c4b1c03ea765308f938d23bfff6b01829afbd
조승기 <beenzino@kookmin.ac.kr>
2024-11-15 16:24:45 +0900

    chore: 공공 API dto 반영

--- backend/route/src/main/java/com/gilin/route/global/client/publicAPI/request/GyeonggiBusArrivalRequest.java
+++ backend/route/src/main/java/com/gilin/route/global/client/publicAPI/request/GyeonggiBusArrivalRequest.java
@@ -1,6 +1,11 @@
 package com.gilin.route.global.client.publicAPI.request;
 
-public record GyeonggiBusArrivalRequest(
-    String serviceKey,
-    String stationId
-) {}
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GyeonggiBusArrivalRequest {
+    String serviceKey;
+    String stationId;
+}
