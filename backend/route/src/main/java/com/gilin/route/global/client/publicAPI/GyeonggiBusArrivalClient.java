4b613c0ab3a7571ec81444a0b8825b4022d52374
조승기 <beenzino@kookmin.ac.kr>
2024-11-15 11:10:38 +0900

    chore: 경기 버스 서울 버스 패키지 구분

--- backend/route/src/main/java/com/gilin/route/global/client/publicAPI/GyeonggiBusArrivalClient.java
+++ backend/route/src/main/java/com/gilin/route/global/client/publicAPI/GyeonggiBusArrivalClient.java
@@ -1,7 +1,7 @@
 package com.gilin.route.global.client.publicAPI;
 
 import com.gilin.route.global.client.publicAPI.request.GyeonggiBusArrivalRequest;
-import com.gilin.route.global.client.publicAPI.response.GyeonggiBusArrivalResponse;
+import com.gilin.route.global.client.publicAPI.response.gyeonggi.GyeonggiBusArrivalResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
